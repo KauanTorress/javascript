0512de5a8c2e2e0bb74218c4feb8619a2eee825c
KauanTorress <kauancamargo.trs@gmail.com>
2025-01-27 20:37:33 -0300

    criando uma calculadora

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-window.alert('Olá')
+function gerarTabuada() {
+    var num = Number(document.querySelector('#tnum').value)
+    var res = document.querySelector('#res')
+
+    //Limpar resultado anterior
+    res.value = ""
+
+    if (isNaN(num)) {
+        res.value = 'Por favor, digite um número válido!'
+    }
+
+    //Gerar tabuada
+    for (var i = 1; i <= 10; i++) {
+        res.value += `${num} x ${i} = ${num * i}\n`
+    }
+}
